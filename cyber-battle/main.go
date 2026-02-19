69fcecd02b6ee0d844f8723c63e7abae5f886b53
EJHacks <ee1912136@gmail.com>
2026-02-18 22:05:54 -0500

    Refactor room creation and joining to use API calls

--- cyber-battle/main.go
+++ cyber-battle/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	docs "cyber-battle/internal/docs"
-	"cyber-battle/internal/server"
 	"cyber-battle/internal/updater"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -29,20 +32,49 @@ func menu() {
 
 	switch choice {
 	case 1:
-		server.CreateRoom()
+		// Create a room via API
+		fmt.Print("Enter room name: ")
+		var name string
+		fmt.Scanln(&name)
+		fmt.Print("Enter max players (2-4): ")
+		var maxPlayers int
+		fmt.Scanln(&maxPlayers)
+		fmt.Print("Set a PIN for your room: ")
+		var pin string
+		fmt.Scanln(&pin)
+		reqBody, _ := json.Marshal(map[string]interface{}{
+			"name":       name,
+			"maxPlayers": maxPlayers,
+			"pin":        pin,
+		})
+		resp, err := http.Post("http://localhost:8080/create-room", "application/json", bytes.NewBuffer(reqBody))
+		if err != nil {
+			fmt.Println("Error connecting to API:", err)
+			return
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(body))
 	case 2:
+		// Join a room via API
 		fmt.Print("Enter room PIN: ")
 		var pin string
 		fmt.Scanln(&pin)
 		fmt.Print("Enter your player name: ")
 		var name string
 		fmt.Scanln(&name)
-		joined := server.JoinRoom(pin, server.Client{Name: name})
-		if !joined {
-			fmt.Println("Failed to join room. Check the PIN or if the room is full.")
-		} else {
-			fmt.Println("Successfully joined the room!")
+		reqBody, _ := json.Marshal(map[string]interface{}{
+			"pin":  pin,
+			"name": name,
+		})
+		resp, err := http.Post("http://localhost:8080/join-room", "application/json", bytes.NewBuffer(reqBody))
+		if err != nil {
+			fmt.Println("Error connecting to API:", err)
+			return
 		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(body))
 	case 3:
 		fmt.Println("Goodbye!")
 		os.Exit(0)
